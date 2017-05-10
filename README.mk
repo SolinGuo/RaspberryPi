ab3876b242613f5e907b0ece67600ef9a40ef771
Solin Guo <solin.guo@outlook.com>
2017-05-10 14:15:59 +0800

    Add README.mk Description

--- README.mk
+++ README.mk
@@ -1,1 +1,11 @@
 # RaspberryPi
+
+Auto Build and Install Kernel
+
+Local building
+
+There are two main methods for building the kernel. You can build locally on a Raspberry Pi, which will take a long time; or you can cross-compile, which is much quicker, but requires more setup.
+
+Refrence
+
+https://www.raspberrypi.org/documentation/linux/kernel/building.md
